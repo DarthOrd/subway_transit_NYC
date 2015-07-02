a85f0ed60ce0f5800301fbd241788247a32fa168
DarthOrd <max.a.ordonez@gmail.com>
2015-07-02 17:12:34 +0000

    stuff

--- application_controller.rb
+++ application_controller.rb
@@ -1,7 +1,16 @@
 require 'bundler'
 Bundler.require
 
-require './models/model.rb'
+require './models/subway_model.rb'
 
 class MyApp < Sinatra::Base
+  get '/' do
+    @subway_lines = ["A ", "B ", "C", "D", "E", "F", "G", "J/Z", "L", "M", "N", "Q", "R", "Franklin Shuttle", "42nd Street Shuttle", "Rockaway Shuttle", "1", "2", "3", "4", "5", "6", "7"] 
+    erb :index
+  end
+  post '/results' do
+   
+#     @trains = 
+    erb :results
+  end
 end
