836e19905c118d5b3a99dcd8f5122f7f94bf72f4
tpearce838 <tydpearce@gmail.com>
2015-07-03 13:51:13 +0000

    Update

--- application_controller.rb
+++ application_controller.rb
@@ -10,8 +10,20 @@ class MyApp < Sinatra::Base
   end
   post '/results' do
     @form_data = params[:train]
+    puts @form_data
     if @form_data == "A"
       @information = Train.new("Inwood", "Far Rockaway-Mott, Lefferts Blvd-Ozone, Rockaway Park-116", "24 hours, Lefferts Service 6am", "Lefferts Service 10pm", "Manhattan Express, Late Night Local")
+    elsif @form_data == "B"
+      @information = Train.new("145th Street", "Brighton Beach", "6am", "11pm", "Rush Hour extention to Bedford Park Blvd. Weekday Service Only, No Late Night. Express Brooklyn, Manhattan, Local in Bronx.")
+    elsif @form_data == 
+        elsif @form_data ==
+           elsif @form_data ==
+        elsif @form_data ==
+        elsif @form_data ==
+        elsif @form_data ==
+        elsif @form_data ==
+        elsif @form_data ==
+        
     end
    
     @trains = params[:lines]
